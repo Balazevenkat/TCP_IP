7eef53cd416a6eee8aad452ef47b2914dce4a478
BALAZEVENKAT <137323932+Balazevenkat@users.noreply.github.com>
2026-03-11 19:29:41 +0530

    Create Basic_client.c

---
+++ Basic_client.c
@@ -0,0 +1,68 @@
+/*struct sockaddr_in {
+    sa_family_t     sin_family;     // AF_INET 
+    in_port_t       sin_port;       // Port number 
+    struct in_addr  sin_addr;       // IPv4 address 
+};
+struct in_addr {
+    in_addr_t s_addr;
+};
+*/
+#include"headers.h"
+//./a.out 3000 127.0.0.1 
+int main(int argc,char *argv[]){
+    if(argc<3){
+        puts("input @cmdline : client.exe portNo serverIP");
+        return 0;
+    }
+    puts("creating client socket...");
+    int fd;
+    fd = socket(AF_INET,SOCK_STREAM,0);
+    if(fd<0){
+        perror("socket");
+        return 0;
+    }
+    puts("client socket created successfully");
+    puts("connect:");
+    puts("& initiating 3-way HS for CE with server");
+    struct sockaddr_in addr;
+    int len = sizeof(addr);
+    addr.sin_family = AF_INET;
+    addr.sin_port = htons(atoi(argv[1]));
+    addr.sin_addr.s_addr = inet_addr(argv[2]);
+    if(connect(fd,(struct sockaddr *)&addr,len)<0){
+        perror("connect");
+        return 0;
+    }
+    puts("connected to server");
+    char buf[100];
+    int status;
+    while(1){
+        puts("Enter any msg for server");
+        fgets(buf,100,stdin);
+        buf[strlen(buf)-1]='\0';
+        puts("sending msg to server...");
+        status=send(fd,buf,100,0);
+        if(status<0){
+            perror("send");
+            break;
+        }else{
+            printf("msg sent to server : %s\n",buf);
+            puts("waiting for echo reply...");
+            status=recv(fd,buf,100,0);
+            if(status<0){
+                perror("recv");
+                break;
+            }else if(status==0){
+                puts("server aborted without 3-way HS for CT");
+                break;
+            }else{
+                printf("echo msg recvd from server : %s\n",buf);
+                if(strcmp(buf,"quit")==0){
+                    break;
+                }
+            }
+        }
+    }
+    puts("client going down");
+    close(fd);
+}
